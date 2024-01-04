2b9316c216a9e715bc145e09049b14c5152c4ffb
AnuradhaLiyanage <it20225438@my.sliit.lk>
2024-01-04 12:47:03 +0530

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+const SEARCH_BOX = '//*[@id="searchField1"]';
+
+const ISBN = '9781405125772';
+
+test('Verify the publications/articles search functionality of onlinelibrary.wiley.com', async ({ page }) => {
+  await page.goto('https://onlinelibrary.wiley.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Wiley Online Library | Scientific research articles, journals, books, and reference works", {timeout: 10000});
+
+  await page.locator(SEARCH_BOX).pressSequentially(ISBN);
+
+  await page.keyboard.press('Enter');
+});
+
+
+
+
